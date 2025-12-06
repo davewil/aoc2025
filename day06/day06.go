05b71525e90e5c785ea69c76bd3fe251df94eaff
David Williams <davewil1973@gmail.com>
2025-12-06 08:14:56 +0000

    Day 6 Part 1

--- day06/day06.go
+++ day06/day06.go
@@ -1,20 +1,95 @@
 package main
 
 import (
-    "fmt"
-    utils "github.com/davewil/aoc-utils"
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+
+	utils "github.com/davewil/aoc-utils"
 )
 
-func parseInput(raw string) ([]string, error) { return []string{}, nil }
-func part1(lines []string) int { return 0 }
-func part2(lines []string) int { return 0 }
+type Input struct {
+	operators   []string
+	expressions [][]int
+}
+
+func parseInput(raw string) (Input, error) {
+	lines := utils.ReadLines(raw)
+	data := [][]string{}
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		data = append(data, fields)
+	}
+
+	slices.Reverse(data)
+
+	expressions := [][]int{}
+	for _, line := range data[1:] {
+		expression := []int{}
+		for _, field := range line {
+			num, err := strconv.Atoi(field)
+			if err != nil {
+				return Input{}, err
+			}
+			expression = append(expression, num)
+		}
+		expressions = append(expressions, expression)
+	}
+	input := Input{
+		operators:   data[0],
+		expressions: expressions,
+	}
+	return input, nil
+}
+
+func part1(input Input) int {
+	total := 0
+	for i, op := range input.operators {
+		col := []int{}
+		for _, row := range input.expressions {
+			if i < len(row) {
+				col = append(col, row[i])
+			}
+		}
+
+		switch op {
+		case "+":
+			sum := 0
+			for _, v := range col {
+				sum += v
+			}
+			total += sum
+		case "*":
+			prod := 1
+			for _, v := range col {
+				prod *= v
+			}
+			total += prod
+		default:
+			fmt.Printf("Unknown operator: %s\n", op)
+		}
+
+	}
+	return total
+}
+
+func part2(input Input) int {
+	return 0
+}
 
 func main() {
-    utils.LoadEnv()
-    raw, err := utils.GetPuzzleInput(2025, 6)
-    if err != nil { fmt.Println("Error fetching input:", err); return }
-    lines, err := parseInput(raw)
-    if err != nil { fmt.Println("Error parsing input:", err); return }
-    fmt.Println("Part 1:", part1(lines))
-    fmt.Println("Part 2:", part2(lines))
+	utils.LoadEnv()
+	raw, err := utils.GetPuzzleInput(2025, 6)
+	if err != nil {
+		fmt.Println("Error fetching input:", err)
+		return
+	}
+	lines, err := parseInput(raw)
+	if err != nil {
+		fmt.Println("Error parsing input:", err)
+		return
+	}
+	fmt.Println("Part 1:", part1(lines))
+	fmt.Println("Part 2:", part2(lines))
 }
--- day06/day06_test.go
+++ day06/day06_test.go
@@ -1,31 +1,34 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
-var exampleInput = ``
+var exampleInput = `123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  `
 
 func TestPart1(t *testing.T) {
-    lines, err := parseInput(exampleInput)
-    if err != nil {
-             t.Fatalf("parseInput error: %v", err)
-    }
-    got := part1(lines)
-    expected := 0
-    if got != expected {
-             t.Errorf("part1() = %d; want %d", got, expected)
-    }
+	lines, err := parseInput(exampleInput)
+	if err != nil {
+		t.Fatalf("parseInput error: %v", err)
+	}
+	got := part1(lines)
+	expected := 4277556
+	if got != expected {
+		t.Errorf("part1() = %d; want %d", got, expected)
+	}
 }
 
 func TestPart2(t *testing.T) {
-    lines, err := parseInput(exampleInput)
-    if err != nil {
-             t.Fatalf("parseInput error: %v", err)
-    }
-    got := part2(lines)
-    expected := 0
-    if got != expected {
-             t.Errorf("part2() = %d; want %d", got, expected)
-    }
+	lines, err := parseInput(exampleInput)
+	if err != nil {
+		t.Fatalf("parseInput error: %v", err)
+	}
+	got := part2(lines)
+	expected := 0
+	if got != expected {
+		t.Errorf("part2() = %d; want %d", got, expected)
+	}
 }
